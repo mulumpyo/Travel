8575954486835b9eab76cb2d72e7daaf9412459f
mulumpyo <1119_dh@naver.com>
2025-06-23 17:28:21 +0900

    init

---
+++ Travel/src/main/java/com/groupone/common/FrontController.java
@@ -0,0 +1,51 @@
+package com.groupone.common;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.groupone.control.MainControl;
+
+
+public class FrontController extends HttpServlet {
+	Map<String, Control> map;
+
+	public FrontController() {
+		map = new HashMap<String, Control>();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		/* 대환 */
+		map.put("/main.do", new MainControl()); // 메인페이지
+		
+		/* 은경 */
+		
+		/* 해민 */
+		
+		/* 유경 */
+		
+		/* 이영 */
+
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		
+		String uri = req.getRequestURI();
+		String context = req.getContextPath();
+		String page = uri.substring(context.length());
+		Control sub = map.get(page);
+		sub.exec(req, res);
+
+	}
+}
---
+++ Travel/src/main/java/com/groupone/control/MainControl.java
@@ -0,0 +1,20 @@
+package com.groupone.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.groupone.common.Control;
+
+public class MainControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException {
+		
+		req.getRequestDispatcher("WEB-INF/jsp/index.jsp").forward(req, res);
+	}
+
+}
