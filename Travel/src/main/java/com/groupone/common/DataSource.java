0d8f2fcb5182b36b84850e6a421d43a689848211
mulumpyo <145254729+mulumpyo@users.noreply.github.com>
2025-06-24 08:09:34 +0900

    DataSource.java 업데이트

--- Travel/src/main/java/com/groupone/common/DataSource.java
+++ Travel/src/main/java/com/groupone/common/DataSource.java
@@ -16,7 +16,7 @@ public class DataSource {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		SqlSessionFactory sqlSessionFactory //
+		SqlSessionFactory sqlSessionFactory
 				= new SqlSessionFactoryBuilder().build(inputStream);
 		return sqlSessionFactory;
 	}
