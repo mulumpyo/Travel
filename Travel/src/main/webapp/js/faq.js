930aa5b831ba77497dbb5afb422446956e230ca9
admin <pc@pc.com>
2025-06-24 17:34:36 +0900

    faq 0624

--- Travel/src/main/webapp/js/faq.js
+++ Travel/src/main/webapp/js/faq.js
@@ -1,7 +1,20 @@
 /**
  *  faq.js
  */
-faq.forEach(function(e) {
-	str = ``
+
+document.querySelectorAll('button.faq').forEach(faq => {
+	faq.addEventListener('click', function(e){
+		if(e.target.innerHTML == '➕'){
+		e.target.parentElement.parentElement.nextElementSibling.firstElementChild.style.display = 'block';
+		e.target.innerHTML = '➖';
+		return;
+	} else if(e.target.innerHTML == '➖') {
+		console.log('check');
+		e.target.parentElement.parentElement.nextElementSibling.firstElementChild.style.display = 'none';
+		e.target.innerHTML = '➕';
+		return;
+	}
 	
 })
+})
+
